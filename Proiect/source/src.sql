fc320e018cf3c412fa71676899a5715ba7609fdb
rachdocx <rachieru.gabriel2016@gmail.com>
2025-04-26 13:54:48 +0300

    .

---
+++ Proiect/source/src.sql
@@ -0,0 +1,9 @@
+-- Cream tabelele
+
+CREATE TABLE FUNCTION (
+    id_function NUMBER(3,0) PRIMARY KEY,
+    function_name VARCHAR2(100),
+    min_salary NUMBER(6,2),
+    max_salary NUMBER(6,2)
+);
+
